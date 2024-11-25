3f60689b81109179a0e0007ac8df4365887e4de7
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-25 17:42:08 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages
+import ErrorPage from "./pages/ErrorPage";
+import Chat from "./pages/Chat";
+import Auth from "./pages/Auth";
+
+function App() {
+  const [createdBy, setCreatedBy] = useState(window.localStorage.getItem("createdBy"));
+  const routes = createBrowserRouter([
+    createdBy
+      ? {
+          path: "/",
+          element: <Chat />,
+          errorElement: <ErrorPage />,
+          children: [],
+        }
+      : {
+          path: "/",
+          element: <Auth />,
+          errorElement: <ErrorPage />,
+          children: [],
+        },
+  ]);
+  useEffect(()=>{},[console.log()])
+  return <RouterProvider router={routes} />;
+}
+
+export default App;
